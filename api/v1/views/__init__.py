99091542911db8731b9766e30f36487fdbdc6dbe
Isaiah woko <wokoisaiah68@gmail.com>
2024-08-03 16:00:02 +0100

    task 3

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -3,7 +3,7 @@
 
 from flask import Blueprint
 
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
+app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
 
-# Wildcard import of everything from the package api.v1.views.index
-from .index import *
+# Wildcard import of everything
+from api.v1.views.index import *
