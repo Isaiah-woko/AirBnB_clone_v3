59b19b39aa8f9a73f171f9e434f1b9fc27079675
Isaiah woko <wokoisaiah68@gmail.com>
2024-08-03 21:03:40 +0100

    task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -14,7 +14,7 @@ def retrieve_all_state():
     list_of_state = []
     state_obj = storage.all("State")
     for obj in state_obj.values():
-        list_of_state.append(obj.to_json())
+        list_of_state.append(obj.to_dict())
 
     return jsonify(list_of_state)
 
@@ -26,7 +26,7 @@ def retrieve_state_id(state_id):
     if get_state_obj is None:
         abort(404)
 
-    return jsonify(get_state_obj.to_json())
+    return jsonify(get_state_obj.to_dict())
 
 
 @app_views.route('/api/v1/states/<state_id>', methods=['DELETE'], strict_slashes=False)
@@ -53,7 +53,24 @@ def create_state():
 
     new_state = State(**json_for_state)
     new_state.save
-    response = jsonify(new_state.to_json())
+    response = jsonify(new_state.to_dict())
     response.status_code = 201
 
     return response
+
+
+@app_views.route('/api/v1/states/<state_id>', methods=['PUT'], strict_slashes=False)
+def update_state(state_id):
+    """Updates a State object"""
+    json_for_state = request.get_json(silent=True)
+    if json_for_state is None:
+        abort(404, 'Not a JSON')
+    get_state_obj = storage.get("State", str(state_id))
+
+    if get_state_obj is None:
+        abort(404)
+    for key, value in json_for_state.items():
+        if key not in ["id", "created_at", "updated_at"]:
+            setattr(get_state_obj, key, value)
+        get_state_obj.save()
+        return jsonify(get_state_obj.to_dict())
