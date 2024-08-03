e744d9ff07c7c037751bd87e26aa21180dfd0460
Isaiah woko <wokoisaiah68@gmail.com>
2024-08-03 19:58:37 +0100

    changes

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,7 +4,7 @@
 from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
-from os import environ
+from os import getenv
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -30,7 +30,7 @@ def error_404(exception):
 
 if __name__ == "__main__":
 
-    host = environ.get('HBNB_API_HOST', '0.0.0.0')
-    port = int(environ.get('HBNB_API_PORT', '5000'))
+    host = getenv("HBNB_API_HOST")
+    port = getenv("HBNB_API_PORT")
 
     app.run(host=host, port=port, threaded=True)
