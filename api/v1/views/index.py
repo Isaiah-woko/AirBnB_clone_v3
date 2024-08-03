1fb6835bb725ab97ada5348977d12380de878cff
Isaiah woko <wokoisaiah68@gmail.com>
2024-08-03 16:00:02 +0100

    task 3

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -2,10 +2,10 @@
 """a script for creating the /status route"""
 
 from flask import jsonify
-from . import app_views
+from api.v1.views import app_views
 
 
-@app_views.route('/status', methods=['GET'], strict_slashes=False)
+@app_views.route("/status", methods=['GET'], strict_slashes=False)
 def get_status():
     """Returns the status"""
     status = {
