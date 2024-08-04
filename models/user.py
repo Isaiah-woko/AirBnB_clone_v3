969ce090112f89d8cf6ecbf7b4b7c41e25efa96c
Isaiah woko <wokoisaiah68@gmail.com>
2024-08-04 20:33:49 +0100

    added md5

--- models/user.py
+++ models/user.py
@@ -6,6 +6,7 @@ from os import getenv
 import sqlalchemy
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
+from hashlib import md5
 
 
 class User(BaseModel, Base):
@@ -27,3 +28,14 @@ class User(BaseModel, Base):
     def __init__(self, *args, **kwargs):
         """initializes user"""
         super().__init__(*args, **kwargs)
+
+    @property
+    def get_password(self):
+        """getter for the password and returns hashed md5"""
+        return self.__dict__.get("password")
+
+    @get_password.setter
+    def hashed_password(self, get_password):
+        """set the password with md5"""
+        self.__dict__["password"] = md5(get_password.encode
+                                        ('utf-8')).hexdigest()
